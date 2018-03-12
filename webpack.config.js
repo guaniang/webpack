eef40a6f3f32a0a06c213b67bb29fc0cb4ad79e9
guanxiang <1563366180@qq.com>
2018-03-12 11:51:14 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: {
+        index:"./src/js/index.js",
+        jiuye:"./src/js/jiuye.js",
+          guanyu:"./src/js/guanyu.js"
+    },
+    //entry==>入口
+    output: {
+        path: __dirname + "/public",
+        filename: "./js/[name][hash].js"
+    },
+    devServer: {
+        contentBase:"./public",//本地服务器所加载的页面所在的目录
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: "style-loader",
+                  use: {
+                  	loader:'css-loader',
+                  	options:{
+                  		minimize:true
+                  	}
+                  }
+                })
+            },
+            {
+            	test:/\.html$/,
+            	loader:'html-withimg-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [{
+                    loader:'url-loader?name=img/[name].[ext]'
+                }]
+            }
+
+        ]
+    },   
+    //出口
+    plugins:[
+        new ExtractTextPlugin("./css/[name].css"),
+        new HtmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'./index.html',
+            chunks:['index'],
+            hash:true,
+            minify:{
+                removeAttributeQuotes: true, // 移除属性的引号
+                caseSensitive: false, //是否大小写敏感
+                removeComments:true, // 去除注释
+                removeEmptyAttributes:true, // 去除空属性
+                collapseWhitespace: true //是否去除空格
+            }
+
+        }),
+        new HtmlWebpackPlugin({
+            template:'./src/guanyu.html',
+            filename:'./guanyu.html',
+            chunks:['guanyu'],
+            hash:true,
+            minify:{
+                removeAttributeQuotes: true, // 移除属性的引号
+                caseSensitive: false, //是否大小写敏感
+                removeComments:true, // 去除注释
+                removeEmptyAttributes:true, // 去除空属性
+                collapseWhitespace: true //是否去除空格
+            }
+
+        }),
+        new HtmlWebpackPlugin({
+            template:'./src/jiuye.html',
+            filename:'./jiuye.html',
+            chunks:['jiuye'],
+            hash:true,
+            minify:{
+                removeAttributeQuotes: true, // 移除属性的引号
+                caseSensitive: false, //是否大小写敏感
+                removeComments:true, // 去除注释
+                removeEmptyAttributes:true, // 去除空属性
+                collapseWhitespace: true //是否去除空格
+            }
+
+        })
+        //new webpack.optimize.UglifyJsPlugin()
+    ]
+
+}
